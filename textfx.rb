2854b946810362cfb6e4d951c8dee66547f4c9c3
MZ <28135287+Mr-MZ-lab@users.noreply.github.com>
2025-08-03 17:23:57 +0200

    Update textfx.rb

--- textfx.rb
+++ textfx.rb
@@ -1,9 +1,9 @@
 class Textfx < Formula
   desc "Terminal text animation and fun script"
-  homepage "https://github.com/YOUR_USERNAME/homebrew-textfx"
-  url "https://raw.githubusercontent.com/YOUR_USERNAME/homebrew-textfx/main/textfx.sh"
+  homepage "https://github.com/Mr-MZ-lab/homebrew-textfx"
+  url "https://raw.githubusercontent.com/Mr-MZ-lab/homebrew-textfx/main/textfx.sh"
   version "1.0"
-  sha256 "<اینجا SHA256 فایل رو می‌ذاریم>"
+  sha256 "<دقیق SHA256 فایل رو بزن اینجا>"
 
   def install
     bin.install "textfx.sh" => "textfx"
